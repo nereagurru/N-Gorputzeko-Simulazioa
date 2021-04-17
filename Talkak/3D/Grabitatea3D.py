0e7ecb581f5151a0eaaf12a1249ee3d838fdbdbc
nereagurru <78503555+nereagurru@users.noreply.github.com>
2021-04-17 12:16:29 +0200

    Update Grabitatea3D.py

--- Talkak/3D/Grabitatea3D.py
+++ Talkak/3D/Grabitatea3D.py
@@ -13,20 +13,51 @@ import time
 
 
 def axyz(N, G, D, m, x, y, z):
-
     """
-    Grabitatearen ondorioz elkarrekintzak jasatzen dituzten partikulen
-    azelerazioa kalkulatzeko funtzioa 2D-tan.
+        Funtzio honek partikula bakoitzaren grabitatearen ondoriozko
+        azelerazioa kalkulatzen du 3 dimentsiotan.
 
-    ax, ay itzuli
+    Jaso:
+        N: partikula kopurua (int)
+        G: konstante grabitazionala (float)
+        D: partikulen diametroa (float)
+        m: partikulen masak (N tamainako np array-n, elementuak float)
+        x: partikulen x koordenatuko posizioak (N tamainako np array-n,
+                                                elementuak float)
+        y: partikulen y koordenatuko posizioak (N tamainako np array-n,
+                                                elementuak float)
+        z: partikulen z koordenatuko posizioak (N tamainako np array-n,
+                                                elementuak float)
+
+    Itzuli:
+        ax: partikulen x koordenatuko azelerazioak (N tamainako np array-n,
+                                                    elementuak float)
+        ay: partikulen y koordenatuko azelerazioak (N tamainako np array-n,
+                                                    elementuak float)
+        az: partikulen z koordenatuko azelerazioak (N tamainako np array-n,
+                                                    elementuak float)
     """
-    xij = np.transpose(np.ones([N, N])*x) - np.ones([N, N])*x  # xi - xj
+    # Partikulen arteko posizio erlatiboak kalkulatu
+    xij = np.transpose(np.ones([N, N])*x) - np.ones([N, N])*x
     yij = np.transpose(np.ones([N, N])*y) - np.ones([N, N])*y
     zij = np.transpose(np.ones([N, N])*z) - np.ones([N, N])*z
-    R = np.power(np.power(xij, 2) + np.power(yij, 2) + np.power(zij, 2), 0.5)  # R
-    Rlagun = R + np.identity(N)
-    matrixx = np.where(R>D, -G*m*np.power(Rlagun, -3)*xij , 0) 
-    matrixy = np.where(R>D, -G*m*np.power(Rlagun, -3)*yij, 0)
-    matrixz = np.where(R>D, -G*m*np.power(Rlagun, -3)*zij, 0)
+    R = np.power(np.power(xij, 2) + np.power(yij, 2) + np.power(zij, 2), 0.5)
 
-    return np.sum(matrixx, axis=1), np.sum(matrixy, axis=1), np.sum(matrixz, axis=1)
+    # Zatitzaile nulurik ez izateko laguntzailea
+    Rlagun = R + np.identity(N)
+
+    # Talkarik ez badago bi elementuen artean batak besteari eragingo lioken
+    # azelerazioa kalkulatzeko. Talka badago azelerazioa grabitatearen
+    # ondorioz nulua
+    matrixx = np.where(R > D, -G*np.transpose(m)*np.power(Rlagun, -3)*xij, 0)
+    matrixy = np.where(R > D, -G*np.transpose(m)*np.power(Rlagun, -3)*yij, 0)
+    matrixz = np.where(R > D, -G*np.transpose(m)*np.power(Rlagun, -3)*zij, 0)
+
+    # Talka jasaten duten elementuen azelerazioa nulua
+    eps = math.pow(10, -2)
+    lagun = np.where(R+(D+eps)*np.identity(N) > D, True, False)
+    ax = np.prod(lagun, axis=1)*np.sum(matrixx, axis=1)
+    ay = np.prod(lagun, axis=1)*np.sum(matrixy, axis=1)
+    az = np.prod(lagun, axis=1)*np.sum(matrixz, axis=1)
+
+    return ax, ay, az
